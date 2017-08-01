5b2d7fd6322fe36a5f2afb08e2beb63b58721386
harrison <notallama@gmail.com>
2017-08-01 10:16:19 -0400

    rss

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,9 +13,17 @@ TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
+FEED_DOMAIN = SITEURL
+FEED_USE_SUMMARY = True
+
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/categories/%s.rss.xml'
+TAG_FEED_RSS = 'feeds/tags/%s.rss.xml'
+
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS =  ()
